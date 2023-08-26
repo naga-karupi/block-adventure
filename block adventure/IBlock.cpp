a9cffd264d5dc66a5fc5a3cfdcc2ae9d4df9a58c
naga <toritonsuibu0527@gmail.com>
2023-08-26 15:20:20 +0900

    IBlockクラスの実装 operator==の実装

--- block adventure/IBlock.cpp
+++ block adventure/IBlock.cpp
@@ -1,2 +1,25 @@
 ﻿#include "stdafx.h"
 #include "IBlock.h"
+
+IBlock::IBlock(String _block_name, bool _destroyable = false)
+	: block_name(_block_name), destroyable(_destroyable) {
+
+	bool is_same_name = false;
+	for (int i = 0; i < block_list.size(); i++) {
+		if (block_name == block_list[i]) {
+			block_number = i;
+			is_same_name = true;
+			break;
+		}
+	}
+
+	if (not is_same_name) {
+		block_number = block_list.size();
+		block_list.push_back(block_name);
+	}
+}
+
+[[nodiscard]]
+constexpr bool IBlock::operator==(IBlock& block) const noexcept {
+	return this->GetBlockNumber() == block.GetBlockNumber();
+}
--- block adventure/IBlock.h
+++ block adventure/IBlock.h
@@ -1,12 +1,28 @@
 ﻿#pragma once
 
 class IBlock {
+	inline static Array<String> block_list{};
+	size_t block_number;
 
 public:
-	virtual void TouchedEvent() = 0;
-	virtual void RideEvent() = 0;
+	static constexpr Size block_size{100, 100};
+
+	String block_name;
+	bool destroyable;
+
+	IBlock(String , bool);
+	[[nodiscard]]
+	constexpr bool operator == (IBlock&) const noexcept;
+
+	[[nodiscard]]
+	constexpr size_t GetBlockNumber() const noexcept{
+		return block_number;
+	}
+
+	virtual bool Collide() = 0;
+	virtual void TouchEvent() = 0;
 	virtual void DestroyEvent() = 0;
+	virtual void BlockDraw() = 0;
 
-	virtual void BlockDraw() const = 0;
+	IBlock() = delete;
 };
-
